55e1d07fbd6032ccaca992ef458cd84764e7a7b8
Sahil Mondal <sahilmondal9879@gmail.com>
2026-04-03 20:42:22 +0530

    Changed fronted connect link

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,12 +14,14 @@ dotenv.config();
 const app = express();
 
 const port = Number(process.env.PORT ?? 4000);
-const corsOrigin = process.env.CORS_ORIGIN ?? "https://anandam-life.vercel.app/";
+const corsOrigin = "https://anandam-life.vercel.app";
 
 app.use(
   cors({
     origin: corsOrigin,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
   }),
 );
 app.use(express.json({ limit: "1mb" }));
